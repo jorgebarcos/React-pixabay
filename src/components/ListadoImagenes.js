b96ef5eca97294d84261c14624dd3d0a6731c8c6
Jorge Barcos <jorgebarcos88@gmail.com>
2019-09-28 11:32:09 -0300

    Mostrando las im{agenes

--- src/components/ListadoImagenes.js
+++ src/components/ListadoImagenes.js
@@ -2,6 +2,6 @@ import React from 'react';
 import Imagen from './Imagen';
 
 function ListadoImagenes({ imagenes }) {
-	return <div className="col-12 p-5 row">{imagenes.map((imagen) => <Imagen key={imagen.id} image={imagen} />)}</div>;
+	return <div className="col-12 p-5 row">{imagenes.map((imagen) => <Imagen key={imagen.id} imagen={imagen} />)}</div>;
 }
 export default ListadoImagenes;
